e8069f44c4700270b165d60b8afbd9c6fd6e1a0d
Ayushatre05 <ayushkatre1801@gmail.com>
2024-04-09 14:39:28 +0530

    done

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
-from dotenv import load_dotenv # type: ignore
-import google.generativeai as gen_ai # type: ignore
+from dotenv import load_dotenv
+import google.generativeai as gen_ai
 
 load_dotenv()
 
@@ -31,7 +31,9 @@ user_prompt = st.chat_input("Ask anything...")
 if user_prompt:
     st.chat_message("user").markdown(user_prompt)
 
-    gemini_response = st.session_state.chat_session.send_message(user_prompt)
-
-    with st.chat_message("assistant"):
-        st.markdown(gemini_response.text)
+    try:
+        gemini_response = st.session_state.chat_session.send_message(user_prompt)
+        with st.chat_message("assistant"):
+            st.markdown(gemini_response.text)
+    except gen_ai.generation_types.StopCandidateException as e:
+        st.error("Sorry, the model cannot generate a suitable response to your query.")
